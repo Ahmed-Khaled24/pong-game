a31439a55a6995d5df79ccd7eb8376dafeeb5005
Ahmed Khaled <Ahmedkhaled.m24@gmail.com>
2022-09-02 22:28:49 +0200

    feat: link server.js with socket.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const app = require('./app');
 const io = require('socket.io');
+const configSockets = require('./socket');
 const {createServer} = require('http');
 const onConnection = require('./socket');
-const app = require('./app');
 
 const httpServer = createServer(app);
 const socketServer = io(httpServer);
@@ -9,5 +10,4 @@ const socketServer = io(httpServer);
 httpServer.listen(3000, ()=>{
     console.log('server is listening on port 3000');
 });
-
-socketServer.on('connection', onConnection);
+configSockets(socketServer);
